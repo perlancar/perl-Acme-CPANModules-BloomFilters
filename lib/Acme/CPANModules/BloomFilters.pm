b17acb2e556502d77eeaee3acbe9cb31f34a4454
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-05-28 06:38:55 +0700

    Add description

--- lib/Acme/CPANModules/BloomFilters.pm
+++ lib/Acme/CPANModules/BloomFilters.pm
@@ -1,14 +1,27 @@
 package Acme::CPANModules::BloomFilters;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
 our $LIST = {
     summary => "Bloom filter modules on CPAN",
     description => <<'_',
 
-My default, go-to choice is L<Algorithm::BloomFilter>, unless there's a specific
-feature I need from other implementations.
+Bloom filter is a data structure that allows you to quickly check whether an
+element is in a set. Compared to a regular hash, it is much more
+memory-efficient. The downside is that bloom filter can give you false
+positives, although false negatives are not possible. So in essence you can ask
+a bloom filter which item is "possibly in set" or "definitely not in set". You
+can configure the rate of false positives. The larger the filter, the smaller
+the rate. Some examples for application of bloom filter include: 1) checking
+whether a password is in a dictionary of millions of common/compromised
+passwords; 2) checking an email address against leak database; 3) virus pattern
+checking; 4) IP/domain blacklisting/whitelisting.
+
+In Perl, my default go-to choice is <pm:Algorithm::BloomFilter>, unless there's
+a specific feature I need from other implementations.
 
 _
     entries => [
@@ -21,7 +34,7 @@ just take a peek at the source code or the hash object and get the filter there.
 Performance might not be stellar since it's pure-Perl.
 
 _
-            tags => ['category:implementation'],
+            tags => ['implementation'],
         },
         {
             module => 'Bloom16',
@@ -31,7 +44,7 @@ An Inline::C module. Barely documented. Also does not provide filter
 saving/loading methods.
 
 _
-            tags => ['category:implementation'],
+            tags => ['implementation'],
         },
         {
             module => 'Algorithm::BloomFilter',
@@ -41,7 +54,7 @@ XS, made by SMUELLER. Can merge other bloom filters. Provides serialize and
 deserialize methods.
 
 _
-            tags => ['category:implementation'],
+            tags => ['implementation'],
         },
         {
             module => 'Bloom::Scalable',
@@ -51,7 +64,7 @@ Pure-perl module. A little weird, IMO, e.g. with hardcoded filenames. The
 distribution also provides <pm:Bloom::Simple>.
 
 _
-            tags => ['category:implementation'],
+            tags => ['implementation'],
         },
         {
             module => 'Bloom::Simple',
@@ -61,7 +74,7 @@ Pure-perl module. A little weird, IMO, e.g. with hardcoded filenames.
 The distribution also provides <pm:Bloom::Simple>.
 
 _
-            tags => ['category:implementation'],
+            tags => ['implementation'],
         },
         {
             module => 'Bloom::Faster',
@@ -71,6 +84,7 @@ XS module. Serialize/deserialize directly to/from files, no string
 (de)serialization provided.
 
 _
+            tags => ['implementation'],
         },
         {
             module => 'Text::Bloom',
@@ -82,9 +96,11 @@ _
         },
         {
             module => 'App::BloomUtils',
+            category => ['cli'],
         },
         {
             module => 'Bencher::Scenarios::BloomFilters',
+            category => ['benchmark'],
         },
     ],
 };
